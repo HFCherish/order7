2e066a86b01ac7e4d1d8cfb60b0e063338b69b0d
HFCherish <hf_cherish@163.com>
2016-07-22 11:54:01 +0800

    [create product] return 400 and error messages when input is incomplete

--- src/main/java/com/thoughtworks/ketsu/web/ProductApi.java
+++ src/main/java/com/thoughtworks/ketsu/web/ProductApi.java
@@ -1,22 +1,32 @@
 package com.thoughtworks.ketsu.web;
 
 import com.thoughtworks.ketsu.web.jersey.Routes;
+import com.thoughtworks.ketsu.web.validators.FieldNotNullValidator;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.net.URI;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Path("products")
 public class ProductApi {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response create(Map prodInfo,
                            @Context Routes routes) {
+        Map<String, List> nullFields = new FieldNotNullValidator().getNullFields(Arrays.asList("name", "description", "price"), prodInfo);
+        if(nullFields.get("items").size() > 0) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(nullFields).build();
+        }
         return Response.created(routes.productUrl(89l)).build();
     }
 }
--- src/test/java/com/thoughtworks/ketsu/web/ProductApiTest.java
+++ src/test/java/com/thoughtworks/ketsu/web/ProductApiTest.java
@@ -7,8 +7,13 @@ import org.junit.runner.RunWith;
 
 import javax.ws.rs.core.Response;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static com.thoughtworks.ketsu.support.TestHelper.productJsonForTest;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -22,5 +27,20 @@ public class ProductApiTest extends ApiSupport {
 
         assertThat(response.getStatus(), is(201));
         assertThat(response.getLocation().toString(), containsString(productBaseUrl));
+    }
+
+    @Test
+    public void should_400_when_creat_given_incomplet_params() {
+        Response response = post(productBaseUrl, new HashMap());
+
+        assertThat(response.getStatus(), is(400));
+        Map map = response.readEntity(Map.class);
+        assertThat(map.get("items"), is(notNullValue()));
+        List errors = (List) map.get("items");
+        assertThat(errors.size(), is(3));
+        Map nameError = (Map)errors.get(0);
+        assertThat(nameError.get("field"), is("name"));
+        assertThat(nameError.get("message").toString(), containsString("name can not be empty"));
+
     }
 }
